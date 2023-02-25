389dc697b2bf430f692113a070287b23486cccd6
Andrea Sloboda <slobodaandrea@gmail.com>
2023-02-25 21:22:57 +0100

    Initial commit, only properties for connection to database added

---
+++ src/main/java/com/personal/virtualPets/PersonalVirtualPetsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.virtualPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalVirtualPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalVirtualPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/virtualPets/PersonalVirtualPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.virtualPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalVirtualPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
